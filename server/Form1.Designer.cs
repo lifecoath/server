3bd91bbff4cd0cf8b44b939ef2841dbbce0ccb12
Administor <Administor@DESKTOP-IRP2I7I>
2017-06-03 20:20:19 +0800

    添加项目文件。

---
+++ server/Form1.Designer.cs
@@ -0,0 +1,222 @@
+﻿namespace server
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.videoSourcePlayer1 = new AForge.Controls.VideoSourcePlayer();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.connect = new System.Windows.Forms.Button();
+            this.camera = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.richTextBox_message = new System.Windows.Forms.RichTextBox();
+            this.button_Send = new System.Windows.Forms.Button();
+            this.textBox_Input = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            this.toolStrip1.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.videoSourcePlayer1);
+            this.groupBox1.Controls.Add(this.pictureBox1);
+            this.groupBox1.Location = new System.Drawing.Point(685, 32);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(349, 531);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // videoSourcePlayer1
+            // 
+            this.videoSourcePlayer1.Location = new System.Drawing.Point(6, 277);
+            this.videoSourcePlayer1.Name = "videoSourcePlayer1";
+            this.videoSourcePlayer1.Size = new System.Drawing.Size(343, 241);
+            this.videoSourcePlayer1.TabIndex = 4;
+            this.videoSourcePlayer1.Text = "videoSourcePlayer1";
+            this.videoSourcePlayer1.VideoSource = null;
+            this.videoSourcePlayer1.NewFrame += new AForge.Controls.VideoSourcePlayer.NewFrameHandler(this.videoSourcePlayer1_NewFrame);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(6, 20);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(337, 251);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // connect
+            // 
+            this.connect.Location = new System.Drawing.Point(18, 435);
+            this.connect.Name = "connect";
+            this.connect.Size = new System.Drawing.Size(75, 23);
+            this.connect.TabIndex = 2;
+            this.connect.Text = "连接";
+            this.connect.UseVisualStyleBackColor = true;
+            this.connect.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // camera
+            // 
+            this.camera.Location = new System.Drawing.Point(18, 464);
+            this.camera.Name = "camera";
+            this.camera.Size = new System.Drawing.Size(75, 23);
+            this.camera.TabIndex = 1;
+            this.camera.Text = "camera";
+            this.camera.UseVisualStyleBackColor = true;
+            this.camera.Click += new System.EventHandler(this.camera_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.listBox1);
+            this.groupBox2.Location = new System.Drawing.Point(12, 32);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(199, 388);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "在线用户：";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Location = new System.Drawing.Point(6, 20);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(185, 340);
+            this.listBox1.TabIndex = 0;
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButton1});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(1045, 25);
+            this.toolStrip1.TabIndex = 4;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton1.Image")));
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton1.Text = "toolStripButton1";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.richTextBox_message);
+            this.groupBox3.Controls.Add(this.button_Send);
+            this.groupBox3.Controls.Add(this.textBox_Input);
+            this.groupBox3.Location = new System.Drawing.Point(223, 32);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(432, 531);
+            this.groupBox3.TabIndex = 5;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "groupBox3";
+            // 
+            // richTextBox_message
+            // 
+            this.richTextBox_message.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.richTextBox_message.HideSelection = false;
+            this.richTextBox_message.Location = new System.Drawing.Point(6, 20);
+            this.richTextBox_message.Name = "richTextBox_message";
+            this.richTextBox_message.ReadOnly = true;
+            this.richTextBox_message.Size = new System.Drawing.Size(418, 277);
+            this.richTextBox_message.TabIndex = 7;
+            this.richTextBox_message.Text = "";
+            // 
+            // button_Send
+            // 
+            this.button_Send.Location = new System.Drawing.Point(351, 500);
+            this.button_Send.Name = "button_Send";
+            this.button_Send.Size = new System.Drawing.Size(75, 23);
+            this.button_Send.TabIndex = 6;
+            this.button_Send.Text = "发送";
+            this.button_Send.UseVisualStyleBackColor = true;
+            this.button_Send.Click += new System.EventHandler(this.button_Send_Click);
+            // 
+            // textBox_Input
+            // 
+            this.textBox_Input.Location = new System.Drawing.Point(6, 312);
+            this.textBox_Input.Multiline = true;
+            this.textBox_Input.Name = "textBox_Input";
+            this.textBox_Input.Size = new System.Drawing.Size(420, 182);
+            this.textBox_Input.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1045, 582);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.connect);
+            this.Controls.Add(this.camera);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button camera;
+        private System.Windows.Forms.Button connect;
+        private AForge.Controls.VideoSourcePlayer videoSourcePlayer1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton toolStripButton1;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button button_Send;
+        private System.Windows.Forms.TextBox textBox_Input;
+        private System.Windows.Forms.RichTextBox richTextBox_message;
+    }
+}
+
---
+++ server/Form1.cs
@@ -0,0 +1,314 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+using AForge;
+using AForge.Video.DirectShow;
+using System.Drawing.Imaging;
+
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace server
+{
+    public partial class Form1 : Form
+    {
+        private Socket socket;
+
+        #region 图像处理
+
+        private Socket ImageServer;
+        private IPEndPoint IPImageServer;
+        private Thread ImageSendThread;
+        private Thread ImageRecThread;
+        private Thread TcpListen;
+        private byte draw;
+        private FilterInfoCollection videoDevices;
+        private string[] CameraName = new string[2];
+        #endregion
+
+        #region 事件处理
+        private Socket EventSocket;
+        private IPEndPoint IpServer = null;
+        private IPEndPoint IpClient = null;
+        private string IpAdd = string.Empty;
+        private EndPoint ServerPoint;
+        private EndPoint ClientPoint;
+        private Thread EventThread;
+
+        #endregion
+        #region 其他
+        private string UserName = string.Empty;
+        private string Message = string.Empty;
+        private string InfoType = string.Empty;
+        #endregion
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+        #region 连接
+        private void SetImageTcp()
+        {
+            IPImageServer = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 1111);
+            socket=new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            socket.Bind(IPImageServer);
+            socket.Listen(10);
+
+            TcpListen = new Thread(listen);
+            TcpListen.IsBackground = true;
+            TcpListen.Start();
+
+        }
+        private void listen()
+        {
+            ImageServer = socket.Accept();
+            byte[] words = new byte[1024];
+            int Byte = ImageServer.Receive(words);
+
+            MessageBox.Show(Encoding.ASCII.GetString(words, 0, Byte));
+        }
+        private void SetEventUDP()
+        {
+            IpServer = new IPEndPoint(IPAddress.Any,1000);
+            ServerPoint = (EndPoint)IpServer;
+            EventSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            EventSocket.Bind(IpServer);
+
+            EventThread = new Thread(Accepting);
+            EventThread.IsBackground = true;
+            EventThread.Start();
+
+        }
+        private void Accepting()
+        {
+            while (true)
+            {
+                EndPoint point = new IPEndPoint(IPAddress.Any, 0);
+                byte[] RecvByte = new byte[256];
+                EventSocket.Blocking = true;
+                EventSocket.ReceiveFrom(RecvByte, ref point);
+
+
+                JObject json = JObject.Parse(Encoding.UTF8.GetString(RecvByte));
+
+                InfoType = json.SelectToken("InfoType").ToString();
+                
+                if (InfoType == "parameter")
+                {
+                    if (IpAdd == string.Empty)//第一次连接
+                    {
+                        IpAdd = json.SelectToken("IpClient").ToString();
+                        UserName = json.SelectToken("UserName").ToString();
+
+                        IpClient = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 1001);
+                        ClientPoint = (EndPoint)IpClient;
+
+                        AnswerReq(UserName, ClientPoint);
+                    }
+                }
+              
+                else
+                {
+                    Message = json.SelectToken("Message").ToString();
+                    ShowInfomation(Message);
+                    //MessageBox.Show(Message);
+                }
+            }
+             
+        }
+        private void ShowInfomation(string info)
+        {
+            Thread ShowText = new Thread(new ParameterizedThreadStart(UpdaterichTextBox_message));
+            ShowText.Start(info);
+        }
+        private void UpdaterichTextBox_message(object str)
+        {
+            if (richTextBox_message.InvokeRequired)
+            {
+                Action<string> actionDelegate = (x) => { this.richTextBox_message.AppendText(x.ToString()); };
+                this.richTextBox_message.Invoke(actionDelegate, str);
+            }
+            else
+            {
+                this.richTextBox_message.AppendText(str.ToString());
+            }
+        }
+        /// <summary>
+        /// 发送消息
+        /// </summary>
+        private void SendMessage(string info,EndPoint Point)
+        {
+            JObject json = new JObject();
+            json.Add("InfoType", "message");
+            json.Add("Message", info);
+            EventSocket.SendTo(Encoding.UTF8.GetBytes(json.ToString()), Point);
+
+        }
+        /// <summary>
+        /// 回复客户端请求
+        /// </summary>
+        /// <param name="user"></param>
+        /// <param name="Point"></param>
+        private void AnswerReq(string user,EndPoint Point)
+        {
+            JObject AnswerJson = new JObject();
+
+            MessageBoxButtons messButton = MessageBoxButtons.OKCancel;
+            DialogResult dr = MessageBox.Show("客户"+user+"请求连接，是否同意？", "连接请求", messButton);
+            if (dr == DialogResult.OK)//如果点击“确定”按钮
+            {
+                AnswerJson.Add("InfoType", "parameter");
+                AnswerJson.Add("Answer", "yes");
+                EventSocket.SendTo(Encoding.UTF8.GetBytes(AnswerJson.ToString()), Point);
+                SetImageTcp();
+            }
+            else//如果点击“取消”按钮
+            {
+                AnswerJson.Add("InfoType", "parameter");
+                AnswerJson.Add("Answer", "no");
+                EventSocket.SendTo(Encoding.UTF8.GetBytes(AnswerJson.ToString()), Point);
+            }
+        }
+        #endregion
+      
+        /// <summary>
+        /// 视频通话
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void camera_Click(object sender, EventArgs e)
+        {
+
+            loadcamera();
+
+            ImageSendThread = new Thread(new ThreadStart(OpenCamrea));
+            ImageSendThread.IsBackground = true;
+            ImageSendThread.Start();
+
+            ImageRecThread = new Thread(new ThreadStart(getimg));
+            ImageRecThread.IsBackground = true;
+            ImageRecThread.Start();
+        }
+        /// <summary>
+        /// 读取摄像头
+        /// </summary>
+        private void loadcamera()
+        {
+            videoDevices = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            for (int i = 0; i < 2; i++)
+            {
+                CameraName[i] = videoDevices[i].MonikerString;
+            }
+        }
+        /// <summary>
+        /// 打开摄像头
+        /// </summary>
+        private void OpenCamrea()
+        {
+            VideoCaptureDevice videoSource = new VideoCaptureDevice(CameraName[0]);
+            videoSourcePlayer1.VideoSource = videoSource;
+            videoSourcePlayer1.Start();
+        }
+        private void videoSourcePlayer1_NewFrame(object sender, ref Bitmap image)
+        {
+            fuction(image);
+        }
+        private void fuction(Bitmap bitmap)
+        {
+            byte[] data;
+            MemoryStream ms = new MemoryStream();
+            bitmap.Save(ms, ImageFormat.Jpeg);
+            data = new byte[ms.Length + 1];
+            ms.Position = 0;
+            data[0] = draw;
+            ms.Read(data, 1, data.Length - 1);//读取数据流
+            Send(data);//发送流
+        }
+        public void Send(byte[] data)//发送函数
+        {
+            int i, l;
+            byte[] value, temp = data;
+            l = temp.Length;
+            value = new byte[l + 4];
+            for (i = 0; i < l; i++)
+                value[i + 4] = (byte)((temp[i] + i) % 256);
+            for (i = 3; i >= 0; i--)
+            {
+                value[i] = (byte)(l % 256);
+                l /= 256;
+            }
+            ImageServer.Send(value);
+
+        }
+        byte[] Receive()//接收函数
+        {
+            byte[] data = new byte[4];
+            int i, size = 0;
+            for (i = ImageServer.Receive(data); i < 4; i = i + ImageServer.Receive(data, i, 4 - i, SocketFlags.None)) Thread.Sleep(10);//获取四字节头
+            for (i = 0; i < 4; i++) size = size * 256 + data[i];//计算分组大小
+            data = new byte[size];
+            for (i = 0; i < size; i = i + ImageServer.Receive(data, i, size - i, SocketFlags.None)) Thread.Sleep(10);//读取流
+            for (i = 0; i < size; i++) data[i] = (byte)((data[i] - i) % 256);//解密
+            return data;
+        }
+        private void getimg()
+        {
+            try
+            {
+                byte[] data;
+                MemoryStream stream;
+                while (true)
+                {
+                    data = Receive();
+                    if (data.Length > 0 && data[0] == draw)
+                    {
+                        stream = new MemoryStream(data, 1, data.Length - 1);//还原图像的编码流                 
+                        pictureBox1.Image = Image.FromStream(stream);
+                    }
+                }
+            }
+            catch (Exception e1)
+            {
+
+                MessageBox.Show(e1.ToString());
+            }
+        }
+        /// <summary>
+        /// 连接
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SetEventUDP();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            JObject json = new JObject();
+            json.Add("InfoType", "Info");
+            json.Add("Message", "accpet");
+            EventSocket.SendTo(Encoding.UTF8.GetBytes(json.ToString()), ClientPoint);
+        }
+
+        private void button_Send_Click(object sender, EventArgs e)
+        {
+            DateTime time = DateTime.Now;
+            String InputInfo = string.Empty;
+            InputInfo = time + "\r\n" + textBox_Input.Text+"\r\n";
+            richTextBox_message.AppendText(InputInfo);
+
+            SendMessage(InputInfo,ClientPoint);
+        }
+    }
+}
